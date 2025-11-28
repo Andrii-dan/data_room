71cbf5d3f6ea0fb8a01b38449ba5e2020b19ad1c
Andrii Danyliuk <89802969+Andrii-dan@users.noreply.github.com>
2025-11-28 01:49:06 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex items-center justify-center w-screen h-screen bg-neutral-200'>
+      Data Room App
+    </div>
+  );
+}
+
+export default App;
