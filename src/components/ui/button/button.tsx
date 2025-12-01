78811998d6bb85909687dac0dcc4d178723a496a
Andrii Danyliuk <89802969+Andrii-dan@users.noreply.github.com>
2025-12-01 01:26:02 +0100

    feat: enhance Button component with loading state and refactor props

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -2,27 +2,43 @@ import * as React from 'react'
 import { Slot } from '@radix-ui/react-slot'
 import { type VariantProps } from 'class-variance-authority'
 
+import { Spinner } from '@/components/ui/spinner'
 import { cn } from '@/lib/utils'
 
 import { buttonVariants } from './button-variants'
+
+type ButtonProps = React.ComponentProps<'button'> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+    loading?: boolean
+  }
 
 export function Button({
   className,
   variant,
   size,
   asChild = false,
+  loading = false,
+  disabled,
+  children,
   ...props
-}: React.ComponentProps<'button'> &
-  VariantProps<typeof buttonVariants> & {
-    asChild?: boolean
-  }) {
+}: ButtonProps) {
   const Comp = asChild ? Slot : 'button'
 
   return (
     <Comp
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
+      disabled={disabled || loading}
       {...props}
-    />
+    >
+      {loading ? (
+        <>
+          Loading... <Spinner className="text-inherit" />
+        </>
+      ) : (
+        children
+      )}
+    </Comp>
   )
 }
