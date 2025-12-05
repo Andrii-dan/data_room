8fd452b957a56de5ad8c8a6ef19c00a704327ecb
Andrii Danyliuk <andrij.danyliuk@gmail.com>
2025-12-05 03:07:26 +0100

    fix: data room container padding fixed

--- src/components/dashboard/data-room/DataRoom.tsx
+++ src/components/dashboard/data-room/DataRoom.tsx
@@ -58,7 +58,7 @@ export function DataRoom() {
   const isEmpty = !filteredFolders.length && !filteredFiles.length
 
   return (
-    <div className="h-full px-4">
+    <div className="h-full px-4 pt-2 pb-6">
       {isEmpty ? (
         <NoData search={!!searchQuery} />
       ) : viewMode === 'grid' ? (
